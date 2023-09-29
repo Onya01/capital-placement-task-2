b758cb756e2ceb37a88a50245c2fe61cd0674938
Caesar <ushmanp@gmail.com>
2023-09-30 00:05:11 +0100

    Done

--- src/components/sideIcons/UserTag.tsx
+++ src/components/sideIcons/UserTag.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-
 const UserTag = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
+      width="26"
+      height="26"
+      viewBox="0 0 26 26"
       fill="none"
     >
       <path
