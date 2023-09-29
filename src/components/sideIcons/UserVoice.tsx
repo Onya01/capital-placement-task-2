e7eb1f883154d0151887dde6a3727be56d259e66
Caesar <ushmanp@gmail.com>
2023-09-30 00:09:20 +0100

    Done

--- src/components/sideIcons/UserVoice.tsx
+++ src/components/sideIcons/UserVoice.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 const UserVoice = () => {
   return (
     <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
+      width="26"
+      height="26"
+      viewBox="0 0 26 26"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
