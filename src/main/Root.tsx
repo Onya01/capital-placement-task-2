df203c5fd0c42460bbfe98c1e3116358ed7969bd
Caesar <ushmanp@gmail.com>
2023-09-29 17:26:32 +0100

    capital placement

---
+++ src/main/Root.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Tabs } from "antd";
+import LoginIcon from "../components/sideIcons/LoginIcon";
+import HomeIcon from "../components/sideIcons/HomeIcon";
+import UserIcon from "../components/sideIcons/UserIcon";
+import CalendarIcon from "../components/sideIcons/CalenderIcon";
+import ShareIcon from "../components/sideIcons/ShareIcon";
+import FileIcon from "../components/sideIcons/FileIcon";
+import FolderIcon from "../components/sideIcons/FolderIcon";
+import HeartIcon from "../components/sideIcons/HeartIcon";
+import BackIcon from "../components/sideIcons/BackIcons";
+import Home from "../page/home/Home";
+import SettingIcon from "../components/sideIcons/SettingIcon";
+import UserNameIcon from "../components/sideIcons/UserNameIcon";
+
+const Layout: React.FC = () => {
+  const iconsArray = [
+    <LoginIcon />,
+    <HomeIcon />,
+    <UserIcon />,
+    <CalendarIcon />,
+    <ShareIcon />,
+    <FileIcon />,
+    <FolderIcon />,
+    <HeartIcon />,
+    <BackIcon />,
+    <></>,
+    <></>,
+    <></>,
+    <></>,
+    <SettingIcon />,
+    <UserNameIcon />,
+  ];
+  return (
+    <div>
+      <Tabs
+        defaultActiveKey="1"
+        tabPosition="left"
+        items={iconsArray.map((i, index) => {
+          const id = String(index);
+          return {
+            label: i,
+            key: id,
+            disabled:index===9 || index===10 || index===11 || index===12,
+            children: <Home />,
+          };
+        })}
+      />
+    </div>
+  );
+};
+
+export default Layout;
